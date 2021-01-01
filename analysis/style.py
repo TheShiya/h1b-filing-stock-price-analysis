908199ec93f955b4e5e21b293e6c8260fd9dda9c
theshiya <shiya993@gmail.com>
2021-01-01 18:51:01 -0500

    added style.py for stylistic commands

---
+++ analysis/style.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (10, 3.5)
+plt.rcParams["lines.linewidth"] = 2
+plt.rcParams["font.size"] = 12
+plt.rcParams["figure.facecolor"] = "f5f5f5"
+plt.rcParams["axes.titlesize"] = 14
+plt.rcParams["axes.facecolor"] = plt.rcParams["figure.facecolor"]
+plt.rcParams["axes.edgecolor"] = plt.rcParams["figure.facecolor"]
+plt.rcParams["axes.linewidth"] = 0.5
+plt.rcParams["grid.linewidth"] = 0.5
+plt.rcParams["grid.color"] = "d2d2d2"
