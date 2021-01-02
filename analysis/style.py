3bc45909720896c93a00603685512aa54197af65
theshiya <shiya993@gmail.com>
2021-01-01 20:20:50 -0500

    updated notebook

--- analysis/style.py
+++ analysis/style.py
@@ -6,12 +6,13 @@ warnings.filterwarnings("ignore")
 
 plt.style.use("fivethirtyeight")
 plt.rcParams["figure.figsize"] = (10, 3.5)
-plt.rcParams["lines.linewidth"] = 2
+plt.rcParams["lines.linewidth"] = 1.5
 plt.rcParams["font.size"] = 12
-plt.rcParams["figure.facecolor"] = "f5f5f5"
+plt.rcParams["figure.facecolor"] = "f8f8f8"
 plt.rcParams["axes.titlesize"] = 14
 plt.rcParams["axes.facecolor"] = plt.rcParams["figure.facecolor"]
-plt.rcParams["axes.edgecolor"] = plt.rcParams["figure.facecolor"]
-plt.rcParams["axes.linewidth"] = 0.5
-plt.rcParams["grid.linewidth"] = 0.5
-plt.rcParams["grid.color"] = "d2d2d2"
+plt.rcParams["axes.edgecolor"] = 'bbbbbb'
+plt.rcParams["axes.linewidth"] = 1
+plt.rcParams["grid.linewidth"] = 1
+plt.rcParams["grid.linestyle"] = ':'
+plt.rcParams["grid.color"] = 'd2d2d2'
